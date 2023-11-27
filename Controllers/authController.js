82cf18d336d341a6c035e2bac2935eea3d23b02f
mdarf <mdarfinji45@gmail.com>
2023-11-27 10:10:09 +0800

    register done

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,9 +1,72 @@
+import { hashPassword } from "../Helpers/authHelpers";
+
+export const  registerController   =  async    (req,res) => {
+
+      try {
+
+         const {name, email , password, phone, address} = req.body;
+
+        // validation
+
+        if(!name){
+            return res.send({error : "Name is Required"})
+        }
+        if(!email){
+            return res.send({error : "Email is Required"})
+        }
+        if(!password){
+            return res.send({error : "Password is Required"})
+        }
+        if(!phone){
+            return res.send({error : "Phone is Required"})
+        }
+
+        if(!address){
+            return res.send({error : "Phone is Required"})
+        }
+
+
+        const existingUser =  await userModel.findOne( {email})
+
+
+        if(existingUser){
+            return  res.status(200).send({
+                success : true,
+                message : 'Already Registered Please Login '
+            })
+        }
+
+        
+        const hashedPassword = await hashPassword(password)
 
 
 
-export const  registerController   = () => {
+        const user = new userModel({name,email,phone, address, password : hashPassword}).save()
 
 
+
+          
+     res.send(201).send ({
+        success :true,
+        message :"User Registered Successfully",
+        user,
+     })
+
+      }
+ 
+
+   
+
+       catch (error) {
+
+        console.log(error)
+        res.status(500).send({
+            success : false,
+            message : 'Error In Registeration',
+            error
+        }) 
+
+      }
 
 
 
---
+++ Helpers/authHelpers.js
@@ -0,0 +1,28 @@
+
+import bcrypt from "bcrypt";
+
+  export const hashPassword = async (password) => {
+
+    try {
+
+         const saltRounds = 10;
+
+         const hashedPassword = await bycript.hash(password, saltRounds)
+
+          return hashedPassword;
+
+    } catch (error) {
+        
+        console.log(error)
+
+    }
+
+
+  }
+
+
+  export const comparePassword = async (password, hashedPassword) => {
+
+    return bcrypt.compare(password, hashedPassword)
+    
+  }
