09ea0401222436324255b8aff659b8c74ae56efc
mdarf <mdarfinji45@gmail.com>
2024-01-21 20:11:08 +0800

    added createProduct controller

---
+++ Route/ProductRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  brainTreePaymentController,
+  braintreeTokenController,
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productCategoryController,
+  productCountController,
+  productFiltersController,
+  productListController,
+  productPhotoController,
+  realtedProductController,
+  searchProductController,
+  updateProductController,
+} from "../Controllers/productController.js";
+import { isAdmin, requireSignIn } from "../Middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+
+
+
+export default router;
