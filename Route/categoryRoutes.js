d9f68c36c73bbc43ad7b8c35ca95201b9fcd6181
mdarf <mdarfinji45@gmail.com>
2024-01-21 19:27:01 +0800

    get all category single category added

--- Route/categoryRoutes.js
+++ Route/categoryRoutes.js
@@ -2,14 +2,14 @@
 import express from "express";
 
 import { isAdmin, requireSignIn } from "../Middlewares/authMiddleware.js";
-import { createCategoryController, updateCategoryController,  } from "../Controllers/createCatoryController.js";
+import { categoryControlller, createCategoryController, updateCategoryController,  } from "../Controllers/createCatoryController.js";
 
 const router = express.Router()
-router.post("/create-category", requireSignIn, isAdmin, createCategoryController )
 
+router.post("/create-category", requireSignIn, isAdmin, createCategoryController )
 
 router.put("/update-category/:id", requireSignIn, isAdmin, updateCategoryController )
 
-
+router.get("/get-category", requireSignIn,  categoryControlller)
 
 export default router;
